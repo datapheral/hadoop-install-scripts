dfc00d0ac847544fce54620710f2b2eb8d8cb2a6
datapheral <datapheral@users.noreply.github.com>
2014-02-15 00:35:15 -0600

    datapheral-web initialize

---
+++ project/datapheral-web/src/main/java/com/datapheral/web/BaseServlet.java
@@ -0,0 +1,25 @@
+package com.datapheral.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse; 
+ 
+public class BaseServlet extends HttpServlet {
+     
+    private static final long serialVersionUID = 1031422249396784970L;
+ 
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+         
+        resp.setContentType("text/html");
+         
+        PrintWriter out = resp.getWriter();
+        out.print("Datapheral web base servlet");
+        out.flush();
+        out.close();
+    }
+}
