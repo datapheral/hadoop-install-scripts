439ed1b2fae26ee486cb28f59b9338c8f4b85797
datapheral <datapheral@users.noreply.github.com>
2014-02-15 02:05:46 -0600

    .

--- project/datapheral-web/src/main/java/com/datapheral/web/ServletLifeCycle.java
+++ project/datapheral-web/src/main/java/com/datapheral/web/ServletLifeCycle.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 public class ServletLifeCycle extends HttpServlet {
  
     /**
-	 * 
+	 * .
 	 */
 	private static final long serialVersionUID = -9215223022021800799L;
 	private int count;
